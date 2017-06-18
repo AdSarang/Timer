3ab9fb19722fc9deda98e24dba483ace50812f52
Advait <advait@advaits-macbook-pro.local>
2017-06-18 20:00:29 +0530

    Version 1.0

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    var timerCount = 210
+    @IBOutlet weak var tickerTimer: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,56 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func playFunction() {
+        timerCount = timerCount-1
+        if(timerCount>0 && timerCount<210){
+        tickerTimer.text = String(timerCount)
+        }
+        else if(timerCount>210){
+            timerCount=210
+            tickerTimer.text = String(timerCount)
+        }
+        else{
+            tickerTimer.text = "0"
+        }
+    }
+    
+    @IBAction func playButtonPressed(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.playFunction), userInfo: nil, repeats: true)
+    }
 
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+        timer.invalidate()
+    }
 
+    @IBAction func decrementButtonPressed(_ sender: Any) {
+        timerCount=timerCount-10
+        if(timerCount>0){
+            tickerTimer.text = String(timerCount)
+        }
+        else{
+            tickerTimer.text = "0"
+            timer.invalidate()
+
+        }
+    }
+    
+    @IBAction func incrementButtonPressed(_ sender: Any) {
+        timerCount = timerCount + 10
+        if(timerCount>210){
+            tickerTimer.text = "210"
+        }
+        else{
+            tickerTimer.text = String(timerCount)
+        }
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        timerCount = 210
+        tickerTimer.text = String(timerCount)
+    }
+    
+    
 }
 
